fa971693f5a66fa0f96c7cdba4dcaa9263ca02e4
Bianca Laura <biancamakio02@gmail.com>
2025-08-04 15:22:33 +0300

    corrected querysamples

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,7 +1,8 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 author_name = "Author Name Here"
-books_by_author = Book.objects.filter(author__name=author_name)
+author = Author.objects.get(name=author_name)
+books_by_library = Book.objects.filter(author=author)
 print(f"Books by {author_name}: {[book.title for book in books_by_author]}")
 
 library_name = "Library Name Here"
