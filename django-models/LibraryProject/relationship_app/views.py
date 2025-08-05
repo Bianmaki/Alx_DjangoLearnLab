0ccca9ce083250ec027f7da34f767f932f99042a
Bianca Laura <biancamakio02@gmail.com>
2025-08-05 16:24:14 +0300

    woi

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Book
-from .models import Library
 
 def list_books(request):
     books = Book.objects
