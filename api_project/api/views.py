945cf0b8fa473837c02b8c94ba891d75dbfa32c2
Bianca Laura <biancamakio02@gmail.com>
2025-08-11 02:00:17 +0300

    its a fresh start

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,2 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Book  # or from bookshelf.models import Book if it's in another app
+from .serializers import BookSerializer
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
 
