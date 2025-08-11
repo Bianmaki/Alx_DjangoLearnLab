2f1255df4d2e9c4eb320d8cb7e9066b698ada66a
Bianca Laura <biancamakio02@gmail.com>
2025-08-11 03:04:12 +0300

    its a fresh start

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -38,8 +38,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'api'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PEERMISSION_CLASSES': [
+        'rest_framework.permission.IsAuthenticated',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
