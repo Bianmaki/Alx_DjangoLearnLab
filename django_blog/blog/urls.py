f5d73e32f7ab015257a5614ab643fb5528cc7019
Bianca Laura <biancamakio02@gmail.com>
2025-08-23 14:49:23 +0300

    the base.html file was missing

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -20,8 +20,8 @@ urlpatterns = [
     path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
     path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
 
-    path("posts/<int:pk>/comments/new/", add_comment, name="add-comment"),
-    path("comments/<int:pk>/edit/", CommentUpdateView.as_view(), name="comment-update"),
-    path("comments/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment-delete"),
+    path("posts/<int:pk>/comment/new/", add_comment, name="add-comment"),
+    path("comment/<int:pk>/update/", CommentUpdateView.as_view(), name="comment-update"),
+    path("comment/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment-delete"),
     
 ] 
