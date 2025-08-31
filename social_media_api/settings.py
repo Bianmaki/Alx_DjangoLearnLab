d55d1287033a05ff6e528c2fc6e181a0d2da7e06
Bianca Laura <biancamakio02@gmail.com>
2025-08-31 17:30:15 +0300

    Resolved merge conflicts

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -9,10 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost 127.0.0.1 [::1]").split()
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 INSTALLED_APPS = [
